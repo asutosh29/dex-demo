b75bc553abd271b170548089f8c2047ff5ce91d7
Garv Makkar <garv.codes@gmail.com>
2025-12-14 17:27:03 +0530

    initialize web + server + browser extension

---
+++ apps/extension/entrypoints/popup/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-80 p-4 bg-green-500">
+        <h1>Heyyy</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ apps/web/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="w-full h-screen bg-secondary"></div>;
+}
+
+export default App;
