0c93d5257e10ab670eef639b2c4eafb970dbf29c
Garv Makkar <garv.codes@gmail.com>
2026-01-18 04:26:00 +0530

    add layout options for viewing collection items

--- apps/server/src/db/schema/helpers/enums-schema.ts
+++ apps/server/src/db/schema/helpers/enums-schema.ts
@@ -1,6 +1,6 @@
 import { pgEnum } from "drizzle-orm/pg-core";
 
-export const itemTypeEnum = pgEnum("item_type", ["link"]); // for now, just links, later on images and documents
+export const itemTypeEnum = pgEnum("item_type", ["link", "note"]); // for now, links and notes, later on images and documents
 
 export const collectionAccessRoleEnum = pgEnum("collection_access_role", [
   "owner",
